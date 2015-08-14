35704a1dcd2e76f911ccbec7c0992ae7175fb318
Andrew Lombardi <andrew@mysticcoders.com>
2015-08-14 14:45:10 -0700

    fix

--- src/main/java/com/annelieseschools/minecraft/SpeedTicker.java
+++ src/main/java/com/annelieseschools/minecraft/SpeedTicker.java
@@ -1,9 +1,9 @@
 package com.annelieseschools.minecraft;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockSponge;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.BlockPos;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -15,28 +15,12 @@ public class SpeedTicker extends BaseEvent {
 		EntityPlayer player = event.player;
 		World world = event.player.worldObj;
 
-		Block underBlock = world.getBlockState(
-				new BlockPos(((int)Math.floor(player.posX)),
-						((int)Math.floor(player.posY)) - 1,
-						((int)Math.floor(player.posZ)))).getBlock();
-		
-		
-//		System.out.println("blocks:"+underBlock);
-		
-		if(!(underBlock instanceof BlockSponge)) {
-			return;
-		}
-
-		player.jump();
-		player.motionY *= 5;
-		
-		/*
 		ItemStack itemOnFoot = event.player.inventory.armorItemInSlot(0);
 		if (itemOnFoot!=null && Item.getIdFromItem(itemOnFoot.getItem()) == Item.getIdFromItem(Items.leather_boots)) {
 			event.player.capabilities.setPlayerWalkSpeed(5.0f);
 		} else {
 			event.player.capabilities.setPlayerWalkSpeed(0.1f);
 		}
-		*/
+
 	}
 }
--- src/main/java/com/annelieseschools/minecraft/Trampoline.java
+++ src/main/java/com/annelieseschools/minecraft/Trampoline.java
@@ -2,9 +2,7 @@ package com.annelieseschools.minecraft;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSponge;
-import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -20,18 +18,18 @@ public class Trampoline extends BaseEvent {
 		// First thing we need to do is check
 		// what block is under the entity feet
 		//
-		// Block underBlock = getBelowBlock(world, player);
+		 Block underBlock = getBelowBlock(world, player);
 		//
 		// And then if the block is NOT a sponge, we return
 		//
-		// if(!(underBlock instanceof BlockSponge)) {
-		// 		return;
-		// }
+		 if(!(underBlock instanceof BlockSponge)) {
+		 		return;
+		 }
 		//
 		// if it is then our player can jump and that jump will be ... high
 		//
-		//		player.jump();
-		//		player.motionY *= 5;
+				player.jump();
+				player.motionY *= SuperJumpCommand.multiplyFactor;
 
 	}
 }
